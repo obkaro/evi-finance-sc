54a0d02b0d97bb12fee3ed17a78ff903910a381e
obkaro <obkaro@gmail.com>
2022-03-16 23:27:27 -0400

    test crap

--- lib/custom_code/actions/flutter_mono.dart
+++ lib/custom_code/actions/flutter_mono.dart
@@ -1,10 +1,1 @@
-// Automatic FlutterFlow imports
-import '../../backend/backend.dart';
-import '../../flutter_flow/flutter_flow_theme.dart';
-import '../../flutter_flow/flutter_flow_util.dart';
-import 'package:flutter/material.dart';
-// Begin custom action code
 
-Future flutterMono() async {
-  // Add your function code here!
-}
