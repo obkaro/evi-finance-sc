0f6d541c244e1cd717cbdc055d2c3964ccd92982
obkaro <obkaro@gmail.com>
2022-07-12 21:52:56 -0400

    working before scattering

--- lib/backend/schema/serializers.g.dart
+++ lib/backend/schema/serializers.g.dart
@@ -10,10 +10,7 @@ Serializers _$serializers = (new Serializers().toBuilder()
       ..add(AccountsRecord.serializer)
       ..add(BudgetCategoriesRecord.serializer)
       ..add(BudgetsRecord.serializer)
-<<<<<<< HEAD
-=======
       ..add(CategoriesRecord.serializer)
->>>>>>> flutterflow
       ..add(ConstBudgetCategoriesRecord.serializer)
       ..add(ConstInstitutionLogosRecord.serializer)
       ..add(TransactionsRecord.serializer)
