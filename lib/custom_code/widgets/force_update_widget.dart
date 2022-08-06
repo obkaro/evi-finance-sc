02d02285e95e451e26c2a685da9c8ea250e1b920
Reva <obkaro@gmail.com>
2022-08-06 07:29:40 -0400

    error version number is int

--- lib/custom_code/widgets/force_update_widget.dart
+++ lib/custom_code/widgets/force_update_widget.dart
@@ -8,18 +8,19 @@ import '../../flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
 // Begin custom widget code
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ForceUpdateWidget extends StatefulWidget {
   const ForceUpdateWidget({
     Key? key,
     this.width,
     this.height,
-    required this.remoteVersion,
+    //required this.remoteVersion,
   }) : super(key: key);
 
   final double? width;
   final double? height;
-  final String remoteVersion;
+  //final String remoteVersion;
 
   @override
   _ForceUpdateWidgetState createState() => _ForceUpdateWidgetState();
@@ -28,6 +29,34 @@ class ForceUpdateWidget extends StatefulWidget {
 class _ForceUpdateWidgetState extends State<ForceUpdateWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder<List<VersionsRecord>>(
+        stream: queryVersionsRecord(
+          queryBuilder: (versionsRecord) =>
+              versionsRecord.orderBy('releaseDate', descending: true),
+          singleRecord: true,
+        ),
+        builder: (context, snapshot) {
+          // Customize what your widget looks like when it's loading.
+          if (!snapshot.hasData) {
+            // return Center(
+            //   child: SizedBox(
+            //     width: 50,
+            //     height: 50,
+            //     child: SpinKitRing(
+            //       color: FlutterFlowTheme.of(context).primaryColor,
+            //       size: 50,
+            //     ),
+            //   ),
+            // );
+            return Container();
+          }
+          List<VersionsRecord> dashboardVersionsRecordList = snapshot.data!;
+          // Return an empty Container when the document does not exist.
+          if (snapshot.data!.isEmpty) {
+            return Container();
+          }
+          final dashboardVersionsRecord = dashboardVersionsRecordList.first;
+          return Container();
+        });
   }
 }
